c128fe34bdb7cfaa60ebf4f94489c7dce47a7b04
Mohammed Azhar <mohammedazhar@GMG.LOCAL>
2022-10-25 10:57:51 +0530

    commit

--- forms.js
+++ forms.js
@@ -2,20 +2,20 @@ var button;
 var backbutton;
 var formdatas;
 var formelements;
-var Create = /** @class */ (function () {
-    function Create(labelname, type, value) {
+var Form = /** @class */ (function () {
+    function Form(labelname, type, value) {
         this.labelname = labelname;
         this.type = type;
         this.value = value;
     }
-    return Create;
+    return Form;
 }());
 //to display formdatas
-var Dispclass = /** @class */ (function () {
-    function Dispclass(pharray) {
+var CreateFormClass = /** @class */ (function () {
+    function CreateFormClass(pharray) {
         this.dispArray = pharray;
     }
-    Dispclass.prototype.createform = function () {
+    CreateFormClass.prototype.createform = function () {
         var disp = document.querySelector(".form");
         disp.innerHTML = "";
         for (var i = 0; i < this.dispArray.length; i++) {
@@ -44,96 +44,66 @@ var Dispclass = /** @class */ (function () {
             }
         }
     };
-    return Dispclass;
+    return CreateFormClass;
 }());
-function displayformdatas() {
-    formelements.style.display = "none";
-    var submitbutton = document.getElementById('submit');
-    submitbutton.style.display = "none";
-    var fname = document.getElementsByClassName('inputclass');
-    // let lname = document.getElementsByClassName('labelclass');
-    formdatas.innerHTML = "";
-    var arr1 = new Array();
-    var arr2 = new Array();
-    for (var i = 0; i < fname.length; i++) {
-        if (fname[i].checked) {
-            // console.log(fname[i].name);
-            arr1.push(fname[i].name);
-            arr2.push(fname[i].attributes[1].nodeValue);
-            // formdatas.innerHTML+=`${fname[i].name}:${fname[i].attributes[1].nodeValue}<br><br>`
-        }
-        else {
-            // console.log(fname[i].name);
-            // console.log(fname[i].value);
-            arr1.push(fname[i].name);
-            arr2.push(fname[i].value);
-            // formdatas.innerHTML+=`${fname[i].name}:${fname[i].value}<br><br>`
-        }
+var DispformDatas = /** @class */ (function () {
+    function DispformDatas() {
     }
-    console.log(arr1);
-    for (var j = 0; j < arr1.length; j++) {
-        if (arr2[j] != "on") {
-            formdatas.innerHTML += "".concat(arr1[j], ":").concat(arr2[j], "<br><br>");
+    DispformDatas.prototype.displaymethod = function () {
+        formelements.style.display = "none";
+        var submitbutton = document.getElementById('submit');
+        submitbutton.style.display = "none";
+        var fname = document.getElementsByClassName('inputclass');
+        // let lname = document.getElementsByClassName('labelclass');
+        formdatas.innerHTML = "";
+        var arr1 = new Array();
+        var arr2 = new Array();
+        for (var i = 0; i < fname.length; i++) {
+            if (fname[i].checked) {
+                arr1.push(fname[i].name);
+                arr2.push(fname[i].attributes[1].nodeValue);
+            }
+            else {
+                arr1.push(fname[i].name);
+                arr2.push(fname[i].value);
+            }
         }
+        for (var j = 0; j < arr1.length; j++) {
+            if (arr2[j] != "on") {
+                formdatas.innerHTML += "".concat(arr1[j], ":").concat(arr2[j], "<br><br>");
+            }
+        }
+        formdatas.style.display = "block";
+        backbutton = document.getElementById('back');
+        backbutton.style.display = "block";
+    };
+    return DispformDatas;
+}());
+var DispformElements = /** @class */ (function () {
+    function DispformElements() {
     }
-    formdatas.style.display = "block";
-    backbutton = document.getElementById('back');
-    backbutton.style.display = "block";
-}
-// to display back form
-function formmethod() {
-    var _a;
-    formelements = document.getElementsByClassName("form");
-    backbutton = document.getElementById('back');
-    backbutton.style.display = "none";
-    (_a = document.querySelector('form')) === null || _a === void 0 ? void 0 : _a.reset();
-    formdatas = document.querySelector('.output');
-    formdatas.style.display = "none";
-    for (var _i = 0, formelements_1 = formelements; _i < formelements_1.length; _i++) {
-        var element = formelements_1[_i];
-        element.style.display = 'block';
-    }
-    var submitbutton = document.getElementById('submit');
-    submitbutton.style.display = "block";
-}
-//creating forms
-// function createform(Array:any){
-//         var disp=document.querySelector(".form") as HTMLInputElement;
-//         disp.innerHTML="";
-//         for (var i=0;i<Array.length;i++){
-//             var p=Array[i]
-//             if (p.type=="input"){
-//                 disp.innerHTML+=`<label class="labelclass">${p.labelname}</label>
-//                                 <input name="${p.labelname}" class="inputclass" type=${p.value}><br><br>`
-//             }
-//             else if ((p.type)=="select"){
-//                 var s="";
-//                 s+=`<label class="labelclass">${p.labelname}</label>
-//                                     <select class="inputclass" name=${p.labelname}>` 
-//                 for(var j=0;j<p.value.length;j++){
-//                     s+=`<option>${p.value[j]}</option>`
-//                 }
-//                 s+=`</select><br><br>`
-//                 disp.innerHTML+=s;
-//             }
-//             else if((p.type)=="radio"){
-//                     var s=""
-//                     s+=`<label class="labelclass">${p.labelname}</label>`
-//                         for(var j=0;j<p.value.length;j++){
-//                             // var x = j.toString()
-//                                     s+=`<input type="radio" id ="${p.value[j]}"name="${p.labelname}" class="inputclass" name=${p.labelname}>
-//                                     <label>${p.value[j]}</label>`
-//                                 }
-//                         s+=`<br><br>`
-//                         disp.innerHTML+=s;
-//                 }
-//             }
-//         }
+    DispformElements.prototype.displaymethod = function () {
+        var _a;
+        formelements = document.getElementsByClassName("form");
+        backbutton = document.getElementById('back');
+        backbutton.style.display = "none";
+        (_a = document.querySelector('form')) === null || _a === void 0 ? void 0 : _a.reset();
+        formdatas = document.querySelector('.output');
+        formdatas.style.display = "none";
+        for (var _i = 0, formelements_1 = formelements; _i < formelements_1.length; _i++) {
+            var element = formelements_1[_i];
+            element.style.display = 'block';
+        }
+        var submitbutton = document.getElementById('submit');
+        submitbutton.style.display = "block";
+    };
+    return DispformElements;
+}());
 document.addEventListener("DOMContentLoaded", function () {
     //create form
-    var l = new Dispclass([new Create("firstname", "input", "text"), new Create("lastname", "input", "text"), new Create("password", "input", "password"),
-        new Create("Education Qualification", "select", ["High School", "Under Graduate", "Post Graduate",]),
-        new Create("gender", "radio", ["male", "female"])]);
+    var l = new CreateFormClass([new Form("First Name", "input", "text"), new Form("Last Name", "input", "text"), new Form("Password", "input", "password"),
+        new Form("Education Qualification", "select", ["High School", "Under Graduate", "Post Graduate",]),
+        new Form("Gender", "radio", ["Male", "Female"])]);
     l.createform();
     //define backbutton
     backbutton = document.getElementById('back');
@@ -143,11 +113,13 @@ document.addEventListener("DOMContentLoaded", function () {
     button === null || button === void 0 ? void 0 : button.addEventListener('click', function handleClick(event) {
         formelements = document.getElementById('form');
         //to display formdatas
-        displayformdatas();
+        var dispdatas = new DispformDatas();
+        dispdatas.displaymethod();
     });
     backbutton = document.getElementById('back');
     backbutton.addEventListener('click', function handleClick(event) {
-        //to display form elemetns
-        formmethod();
+        //to display form elements
+        var dispform = new DispformElements();
+        dispform.displaymethod();
     });
 });
--- forms.ts
+++ forms.ts
@@ -1,39 +1,44 @@
 
-var button:any;
-var backbutton:any;
-var formdatas:any;
-var formelements:any;
+var button: any;
+var backbutton: any;
+var formdatas: any;
+var formelements: any;
 
 
 
 
 
-interface ICreate{
-    labelname:string;
-    type:string;
-    value?:any;
+interface IFormElements {
 
+    labelname: string;
+    type: string;
+    value?: any;
 }
 
 
 
-class Create implements ICreate{
-    labelname:string;
-    type:string;
-    value?:any;
+class Form implements IFormElements {
+
+    labelname: string;
+    type: string;
+    value?: any;
 
 
-    constructor(labelname:string,type:string,value:any){
-        this.labelname=labelname;
-        this.type=type;
-        this.value=value;
+    constructor(labelname: string, type: string, value: any) {
+        this.labelname = labelname;
+        this.type = type;
+        this.value = value;
+
     }
+
 }
 
 
-interface IDisp{
-    dispArray:Array<Create>
+interface ICreate {
+
+    dispArray: Array<Form>
     createform();
+
 }
 
 
@@ -41,207 +46,179 @@ interface IDisp{
 //to display formdatas
 
 
-class Dispclass implements IDisp{
-    dispArray:Array<Create>
-    constructor(pharray:any){
-        this.dispArray=pharray
+class CreateFormClass implements ICreate {
+
+    dispArray: Array<Form>
+
+    constructor(pharray: any) 
+    {
+        this.dispArray = pharray
     }
 
-     createform(){
-            var disp=document.querySelector(".form") as HTMLInputElement;
-            disp.innerHTML="";
-            for (var i=0;i<this.dispArray.length;i++){
-                var p=this.dispArray[i]
-                if (p.type=="input"){
-      
-                    disp.innerHTML+=`<label class="labelclass">${p.labelname}</label>
+    createform() {
+
+        var disp = document.querySelector(".form") as HTMLInputElement;
+        disp.innerHTML = "";
+        for (var i = 0; i < this.dispArray.length; i++) {
+
+            var p = this.dispArray[i]
+
+            if (p.type == "input") {
+
+                disp.innerHTML += `<label class="labelclass">${p.labelname}</label>
                                     <input name="${p.labelname}" class="inputclass" type=${p.value}><br><br>`
-                    
+
+            }
+
+            else if ((p.type) == "select") {
+
+                var s = "";
+                s += `<label class="labelclass">${p.labelname}</label>
+                                        <select class="inputclass" name=${p.labelname}>`
+
+                for (var j = 0; j < p.value.length; j++) {
+
+                    s += `<option>${p.value[j]}</option>`
                 }
-    
-                else if ((p.type)=="select"){
-                    var s="";
-                    s+=`<label class="labelclass">${p.labelname}</label>
-                                        <select class="inputclass" name=${p.labelname}>` 
-      
-                    for(var j=0;j<p.value.length;j++){
-                        s+=`<option>${p.value[j]}</option>`
-                    }
-                    s+=`</select><br><br>`
-                    disp.innerHTML+=s;
-    
-                }
-    
-                else if((p.type)=="radio"){
-                    
-                        var s=""
-                        s+=`<label class="labelclass">${p.labelname}</label>`
-    
-                            for(var j=0;j<p.value.length;j++){
-                                // var x = j.toString()
-                                        s+=`<input type="radio" id ="${p.value[j]}"name="${p.labelname}" class="inputclass" name=${p.labelname}>
+                s += `</select><br><br>`
+                disp.innerHTML += s;
+
+            }
+
+            else if ((p.type) == "radio") {
+
+                var s = ""
+                s += `<label class="labelclass">${p.labelname}</label>`
+
+                for (var j = 0; j < p.value.length; j++) {
+                    // var x = j.toString()
+                    s += `<input type="radio" id ="${p.value[j]}"name="${p.labelname}" class="inputclass" name=${p.labelname}>
                                         <label>${p.value[j]}</label>`
-                                    }
-                            s+=`<br><br>`
-                            disp.innerHTML+=s;
-                    }
-    
                 }
-            }   
+                s += `<br><br>`
+                disp.innerHTML += s;
+            }
+
+        }
+    }
+}
+
+interface IDispForm {
+
+    displaymethod()
+
+}
+
+
+class DispformDatas implements IDispForm {
+
+    displaymethod() {
+
+        formelements.style.display = "none";
+        let submitbutton = <HTMLInputElement>document.getElementById('submit');
+
+
+        submitbutton.style.display = "none"
+
+        let fname = document.getElementsByClassName('inputclass');
+        // let lname = document.getElementsByClassName('labelclass');
+        formdatas.innerHTML = "";
+
+
+        let arr1 = new Array()
+        let arr2 = new Array()
+
+        for (var i = 0; i < fname.length; i++) {
+
+            if (fname[i].checked) {
+
+                arr1.push(fname[i].name)
+                arr2.push(fname[i].attributes[1].nodeValue)
+            }
+            else {
+
+                arr1.push(fname[i].name)
+                arr2.push(fname[i].value)
+
+            }
+
+
+        }
+
+        for (var j = 0; j < arr1.length; j++) {
+            if (arr2[j] != "on") {
+                formdatas.innerHTML += `${arr1[j]}:${arr2[j]}<br><br>`
+
+            }
+        }
+
+        formdatas.style.display = "block";
+        backbutton = document.getElementById('back');
+        backbutton.style.display = "block"
     }
 
-function displayformdatas(){
+}
 
-  
 
-    formelements.style.display="none";
-    let submitbutton= <HTMLInputElement>document.getElementById('submit');
-    
-    
-    submitbutton.style.display="none"
+class DispformElements implements IDispForm {
 
-    let fname = document.getElementsByClassName('inputclass');
-    // let lname = document.getElementsByClassName('labelclass');
-    formdatas.innerHTML="";
+    displaymethod() {
 
-   
-    let arr1  = new Array()
-    let arr2 = new Array()
-    for (var i=0;i<fname.length;i++){
-    
-        if(fname[i].checked){
-            // console.log(fname[i].name);
-            
-            arr1.push(fname[i].name)
-            arr2.push(fname[i].attributes[1].nodeValue)
-            // formdatas.innerHTML+=`${fname[i].name}:${fname[i].attributes[1].nodeValue}<br><br>`
+        formelements = document.getElementsByClassName("form");
+        backbutton = document.getElementById('back');
+        backbutton.style.display = "none"
+
+        document.querySelector('form')?.reset();
+
+        formdatas = document.querySelector('.output') as HTMLInputElement;
+        formdatas.style.display = "none";
+
+        for (const element of formelements) {
+
+            element.style.display = 'block';
+        }
         
-        }
-        else{
-            // console.log(fname[i].name);
-            // console.log(fname[i].value);
-            arr1.push(fname[i].name)
-            arr2.push(fname[i].value)
-            // formdatas.innerHTML+=`${fname[i].name}:${fname[i].value}<br><br>`
-      
-        }
-                
-      
+        let submitbutton = <HTMLInputElement>document.getElementById('submit');
+        submitbutton.style.display = "block"
+
+
     }
-
-    for (var j=0;j<arr1.length;j++){
-        if (arr2[j]!="on"){
-            formdatas.innerHTML+=`${arr1[j]}:${arr2[j]}<br><br>`
-
-        }
-    }
-
-    formdatas.style.display="block";
-  backbutton = document.getElementById('back');
-  
-  
-  backbutton.style.display="block"}
+}
 
 
 
-// to display back form
-  
-function formmethod(){
-    formelements= document.getElementsByClassName("form");
-    backbutton = document.getElementById('back');
-    backbutton.style.display="none"
-
-    document.querySelector('form')?.reset();
-    
-    formdatas= document.querySelector('.output') as HTMLInputElement;
-    formdatas.style.display="none";
-  
-    for (const element of formelements) {
-      element.style.display = 'block';
-    }
-    let submitbutton= <HTMLInputElement>document.getElementById('submit');
-    submitbutton.style.display="block"
-    
-  
-  }
-
-
-  //creating forms
-
-// function createform(Array:any){
-//         var disp=document.querySelector(".form") as HTMLInputElement;
-//         disp.innerHTML="";
-//         for (var i=0;i<Array.length;i++){
-//             var p=Array[i]
-//             if (p.type=="input"){
-  
-//                 disp.innerHTML+=`<label class="labelclass">${p.labelname}</label>
-//                                 <input name="${p.labelname}" class="inputclass" type=${p.value}><br><br>`
-                
-//             }
-
-//             else if ((p.type)=="select"){
-//                 var s="";
-//                 s+=`<label class="labelclass">${p.labelname}</label>
-//                                     <select class="inputclass" name=${p.labelname}>` 
-  
-//                 for(var j=0;j<p.value.length;j++){
-//                     s+=`<option>${p.value[j]}</option>`
-//                 }
-//                 s+=`</select><br><br>`
-//                 disp.innerHTML+=s;
-
-//             }
-
-//             else if((p.type)=="radio"){
-                
-//                     var s=""
-//                     s+=`<label class="labelclass">${p.labelname}</label>`
-
-//                         for(var j=0;j<p.value.length;j++){
-//                             // var x = j.toString()
-//                                     s+=`<input type="radio" id ="${p.value[j]}"name="${p.labelname}" class="inputclass" name=${p.labelname}>
-//                                     <label>${p.value[j]}</label>`
-//                                 }
-//                         s+=`<br><br>`
-//                         disp.innerHTML+=s;
-//                 }
-
-//             }
-//         }
-
-
-
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
     //create form
-    let l=new Dispclass([new Create("firstname","input","text"),new Create("lastname","input","text"),new Create("password","input","password"),
-    new Create("Education Qualification","select",["High School","Under Graduate","Post Graduate",]),
-    new Create ("gender","radio",["male","female"])])
+    let l = new CreateFormClass([new Form("First Name", "input", "text"), new Form("Last Name", "input", "text"), new Form("Password", "input", "password"),
+    new Form("Education Qualification", "select", ["High School", "Under Graduate", "Post Graduate",]),
+    new Form("Gender", "radio", ["Male", "Female"])])
     l.createform();
 
 
     //define backbutton
 
     backbutton = document.getElementById('back');
-    backbutton.style.display="none"
+    backbutton.style.display = "none"
     button = document.getElementById('submit');
-    formdatas= document.querySelector('.output') as HTMLInputElement;
+    formdatas = document.querySelector('.output') as HTMLInputElement;
 
-  button?.addEventListener('click', function handleClick(event) {
-    formelements= <HTMLInputElement>document.getElementById('form');
+    button?.addEventListener('click', function handleClick(event) {
+        formelements = <HTMLInputElement>document.getElementById('form');
 
-    //to display formdatas
-    displayformdatas()});
+        //to display formdatas
+        let dispdatas = new DispformDatas()
+        dispdatas.displaymethod()
+    });
 
-  backbutton = document.getElementById('back');
-  backbutton.addEventListener('click', function handleClick(event) {
-    
-    //to display form elemetns
-    
-    formmethod()});
+    backbutton = document.getElementById('back');
+    backbutton.addEventListener('click', function handleClick(event) {
 
+        //to display form elements
+        let dispform = new DispformElements()
+        dispform.displaymethod()
 
+    });
 
 })
 
